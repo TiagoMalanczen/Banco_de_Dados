ceed32a2968b77967ec59f01e00435ac39987009
Tiago Malanczen <malanczen13@gmail.com>
2026-05-26 16:59:16 -0300

    (feat) : fazendo o povoamento das tabelas criadas

--- AprendendoSQL/Comandos_Basicos.sql
+++ AprendendoSQL/Comandos_Basicos.sql
@@ -14,7 +14,6 @@ CREATE TABLE DEV(
     PRIMARY KEY(ID_DEV)
 );
 
-
 CREATE TABLE PROJETO(
     ID_PROJETO INT AUTO_INCREMENT,
     NOME_PROJETO VARCHAR(100) NOT NULL,
@@ -27,10 +26,22 @@ CREATE TABLE PROJETO(
 SHOW TABLES;
 DESC DEV;
 DESC PROJETO;
+
 -- =========================================================================
 -- BLOCO 3: MANIPULAÇÃO E POVOAMENTO DE DADOS (DML)
 -- =========================================================================
+INSERT INTO DEV(NOME, LINGUAGEM)
+VALUES  ("Tiago Silva", "PHP"),
+        ("Marcos Santos", "Java");
+INSERT INTO DEV(NOME)
+VALUES  ("Aline Souza");
 
+INSERT INTO PROJETO(NOME_PROJETO, ORCAMENTO, ID_DEV)
+VALUES  ("Sistemas E-commerce", 1500.00, 1),
+        ("App Mobile", 8500.00, 2);
+
+SELECT *FROM DEV;
+SELECT *FROM PROJETO;
 -- =========================================================================
 -- BLOCO 4: CONSULTAS, FILTROS E JUNÇÕES (DQL)
 -- =========================================================================
